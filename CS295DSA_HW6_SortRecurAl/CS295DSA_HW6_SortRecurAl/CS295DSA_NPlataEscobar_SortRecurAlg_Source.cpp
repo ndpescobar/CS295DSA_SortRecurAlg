98b13fa24c3a27cdc8579e118d7e80df7131498a
ndpescobar <ndpescobar@gmail.com>
2019-08-01 12:02:54 -0700

    Testing a successful build
    Writing merge sort.

--- CS295DSA_HW6_SortRecurAl/CS295DSA_HW6_SortRecurAl/CS295DSA_NPlataEscobar_SortRecurAlg_Source.cpp
+++ CS295DSA_HW6_SortRecurAl/CS295DSA_HW6_SortRecurAl/CS295DSA_NPlataEscobar_SortRecurAlg_Source.cpp
@@ -8,25 +8,38 @@
 	@date: 08/01/2019
 	@version: 0.1
 */
+//------ Include Libraries ------
 #include <iostream>
+#include <string>
 
+//------ Variable/Method Declarations ------
+int arrA[8] = {3,2,1,10,11,12,20,15};
 
+void MergeSort(int arr[], int left, int right);
+void InsertionSort();
 
-
+//------ MAIN ------
 int main() {
 	
 	std::cout << "Double ENTER to exit." << std::endl;
-	string exit;
-	getline(cin, exit);
-	getline(cin, exit);
+	std::string exit;
+	std::getline(std::cin, exit);
+	std::getline(std::cin, exit);
 	std::cout << "Goodbye!";
 	return 0;
+}
+
+//------ Methods ------
+/* Merge Sort
+
+*/
+void MergeSort(int arr[], int left, int right) {
+
 }
 
 /*	Insertion Sort
 
 */
+void InsertionSort() {
 
-/* Merge Sort
-
-*/
+}

adf614c7de7afb32ad66b5bbca5118066b23c0c8
ndpescobar <ndpescobar@gmail.com>
2019-08-01 10:45:04 -0700

    Set up and initialization
    Creating the repository and starting the homework. First commit.

---
+++ CS295DSA_HW6_SortRecurAl/CS295DSA_HW6_SortRecurAl/CS295DSA_NPlataEscobar_SortRecurAlg_Source.cpp
@@ -0,0 +1,32 @@
+/*	CS295 Data Structures and Algorithms
+	Homework #06: Sorting and Recursive Algorithms
+	This assignment is all about learning insertion and merge sort.
+
+	This program will be able to apply insertion and merge sort to any given array,
+	however, one array has been provided.
+	@author: Nicolas Plata-Escobar
+	@date: 08/01/2019
+	@version: 0.1
+*/
+#include <iostream>
+
+
+
+
+int main() {
+	
+	std::cout << "Double ENTER to exit." << std::endl;
+	string exit;
+	getline(cin, exit);
+	getline(cin, exit);
+	std::cout << "Goodbye!";
+	return 0;
+}
+
+/*	Insertion Sort
+
+*/
+
+/* Merge Sort
+
+*/
